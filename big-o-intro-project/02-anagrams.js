b17e1b6b8630ab81554c702811e0d9a70d135138
Keith Mellea <keithmellea@gmail.com>
2021-02-24 18:45:44 -0500

    first commit

--- big-o-intro-project/02-anagrams.js
+++ big-o-intro-project/02-anagrams.js
@@ -37,17 +37,42 @@ function secondAnagram(str1, str2) {
 function thirdAnagram(str1, str2) {
   let obj1 = {};
   let obj2 = {};
-
+debugger
   for (let i = 0; i < str1.length; i++) {
     const char1 = str1[i];
     const char2 = str2[i];
-    obj1[char1]++;
-    obj2[char2]++;
-    if (obj1[char1] !== obj2[char2]) {
+    debugger
+    if (obj1[char1] !== undefined) {
+      debugger
+     obj1[char1]++;
+    }
+    else {
+      debugger
+      obj1[char1] = 1;
+    }
+    debugger
+    if (obj2[char2] !== undefined) {
+      debugger
+      obj2[char2]++;
+    } else {
+      debugger
+      obj2[char2] = 1;
+    }
+    debugger
+   
+  }
+  let obj1Keys = Object.keys(obj1).sort();
+  let obj2Keys = Object.keys(obj2).sort();
+
+  for (let i = 0; i < obj1Keys.length; i++) {
+    if (obj1Keys[i] !== obj2Keys[i]) {
       return false;
     }
   }
-  return true;
+  console.log('obj1:', obj1Keys)
+  console.log("obj2:", obj2Keys);
+
+return true;
 }
 
 console.log(thirdAnagram("gizmo", "sally"));
