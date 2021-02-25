809d3b6553b7a63219a3df70985407c778ec90eb
James Sullivan <jsullivan86@gmail.com>
2021-02-24 16:04:06 -0800

    Sorting TwoSum

--- big-o-intro-project/03-two-sum.js
+++ big-o-intro-project/03-two-sum.js
@@ -1,7 +1,14 @@
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
-}
 
+  for (let i = 0; i < arr.length; i++) {
+    let twoSum = arr[i] + arr[i + 1];
+    if (twoSum === targetSum) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(badTwoSum([1, 2, 3, 4], 7));
 
 function okayTwoSum1(arr, targetSum) {
   // Code goes here ...
